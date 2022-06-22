5f82444019e87441744cf5741208e949fd110ff5
BartoszRamza1 <79877911+BartoszRamza1@users.noreply.github.com>
2022-06-22 22:14:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
+import './App.css'
+import ListaPostaci from './components/ListaPostaci'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [postaci, setPostaci] = useState(null)
+
+
+    fetch(' https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data =>
+        setPostaci(data))
+     
+
+    if (!postaci) {
+        return(<div className="App">Brak danych z backendu</div>)
+    }
+
+  
+
+  return(
+        <div className="App">
+            <ListaPostaci postaci={postaci}/>
+        </div>
+    );
+
+
+
 }
 
-export default App;
+
+export default App
